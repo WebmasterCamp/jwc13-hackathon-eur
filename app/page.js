d29f6bdd263f875738a5dabebb28fb63332018c1
Pronwattana Wattanathamrong <120812215+guzamak@users.noreply.github.com>
2024-07-21 16:50:57 +0700

    font

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 "use client"
 import { useEffect, useState } from "react";
 import Box2 from "./components/box2";
-import Box1 from "./components/box1";
 
 export default function Home() {
 
   return (
-    <main className="flex gap-5 relative">
+    <main className="flex gap-5 relative font-Itim">
       <div className="bg-[#DEF9C4] -z-20 absolute w-full h-full"></div>
       <div className="min-h-screen  flex w-full justify-center mt-52">
         <Box2 salary={250000} />
